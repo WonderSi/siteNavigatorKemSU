75a70c65a4f1ca6130f3cd6f21b35846c67d0fd1
WonderSi <kirillkozlov00@gmail.ru>
2023-10-07 11:18:31 +0700

    Интерактивная карта

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 const menuBtn = document.querySelector('.menu-btn');
 const sideMenu = document.querySelector('.menu');
+const sideMap = document.querySelector('.map');
 const menuBthIcons = document.querySelector('.menu-btn__box_icons');
 
 menuBtn.addEventListener('click', () => {
   sideMenu.classList.toggle('open');
+  sideMap.classList.toggle('open');
   menuBtn.classList.toggle('open');
   menuBthIcons.classList.toggle('rotate-180');
 });
