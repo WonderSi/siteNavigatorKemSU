5fe0e5fea8f39103ef2384e46fdaea64219f282d
Norrim1 <danilbabenko76840@gmail.com>
2023-12-06 18:44:20 +0700

    search_function full working
    Поисковая строка полностью работает и готова к адаптации под всю карту.

--- src/js/search-function.js
+++ src/js/search-function.js
@@ -1,13 +1,18 @@
 
 function SaveInput()
 {
-    image_frame_Home_right.style.display = 'none'
-    image_frame_51.style.display = 'block'
+    event.preventDefault();
     try {
         var inputElement = document.getElementById("search-id");
         var inputValue = inputElement.value;
         console.log(inputValue);
-
+        if (inputValue < 5200 && inputValue > 5000) {
+            image_frame_Home_right.style.display = 'none'
+            image_frame_51.style.display = 'block'
+        } else if (inputValue == "5 корпус 1 этаж") {
+            image_frame_Home_right.style.display = 'none'
+            image_frame_51.style.display = 'block'
+        }
     } catch (error) {
         console.error("An error occurred:", error);
     }
