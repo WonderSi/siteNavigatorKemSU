1b4bb500ad8f57852871a4a355b728fbdb8c1e7b
Norrim1 <danilbabenko76840@gmail.com>
2023-12-11 22:06:11 +0700

    Emergency Search Function Repair

--- src/js/search-function.js
+++ src/js/search-function.js
@@ -7,11 +7,18 @@ function SaveInput()
         var inputValue = inputElement.value;
         console.log(inputValue);
         if (inputValue < 5200 && inputValue > 5000) {
-            image_frame_Home_right.style.display = 'none'
+            image_frame_Home.style.display = 'none'
+            image_frame_24.style.display = 'none'
             image_frame_51.style.display = 'block'
         } else if (inputValue == "5 корпус 1 этаж") {
-            image_frame_Home_right.style.display = 'none'
+            image_frame_Home.style.display = 'none'
+            image_frame_24.style.display = 'none'
             image_frame_51.style.display = 'block'
+        }
+        else if (inputValue > 2400 && inputValue < 2500) {
+            image_frame_Home.style.display = 'none'
+            image_frame_51.style.display = 'none'
+            image_frame_24.style.display = 'block'
         }
     } catch (error) {
         console.error("An error occurred:", error);
