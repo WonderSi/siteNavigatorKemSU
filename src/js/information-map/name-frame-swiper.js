5372775cc1d442d75f1385cd77411902c2248aa9
WonderSi <kirillkozlov00@gmail.com>
2023-12-21 02:36:15 +0700

    cleaning the code
    Сортировка и уборка кода от лишних строк и файлов. Сделано для того, чтобы легче воспринимать код при его чтении.

---
+++ src/js/information-map/name-frame-swiper.js
@@ -0,0 +1,137 @@
+document.getElementById('frame-bth__box__block51').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block52').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block53').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block54').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 4 Этаж";
+});
+
+
+
+document.getElementById('frame-bth__box__block31').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block32').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block33').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block34').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 4 Этаж";
+});
+document.getElementById('frame-bth__box__block35').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 5 Этаж";
+});
+document.getElementById('frame-bth__box__block36').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 6 Этаж";
+});
+
+
+
+document.getElementById('frame-bth__box__block21').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block22').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block23').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block24').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 4 Этаж";
+});
+
+
+
+document.getElementById('frame-bth__box__block11').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block12').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block13').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block14').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 4 Этаж";
+});
+document.getElementById('frame-bth__box__block15').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 5 Этаж";
+});
+
+
+
+
+
+document.getElementById('frame-bth__box__block51(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block52(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block53(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block54(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame5').textContent = "Корпус №5, 4 Этаж";
+});
+
+
+
+document.getElementById('frame-bth__box__block31(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block32(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block33(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block34(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 4 Этаж";
+});
+document.getElementById('frame-bth__box__block35(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 5 Этаж";
+});
+document.getElementById('frame-bth__box__block36(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame3').textContent = "Корпус №3, 6 Этаж";
+});
+
+
+
+document.getElementById('frame-bth__box__block21(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block22(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block23(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block24(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame2').textContent = "Корпус №2, 4 Этаж";
+});
+
+
+
+document.getElementById('frame-bth__box__block11(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 1 Этаж";
+});
+document.getElementById('frame-bth__box__block12(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 2 Этаж";
+});
+document.getElementById('frame-bth__box__block13(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 3 Этаж";
+});
+document.getElementById('frame-bth__box__block14(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 4 Этаж";
+});
+document.getElementById('frame-bth__box__block15(right)').addEventListener('click', function() {
+    document.getElementById('frame_address-frame1').textContent = "Корпус №1, 5 Этаж";
+});
